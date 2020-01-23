376e6c3dc1c45a556165faa23b1b636a9c44e92a
Philip Helger <philip@helger.com>
2020-01-23 14:09:40 +0100

    Added push script

---
+++ smp/publish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+version=5.2.0
+
+docker login
+docker push toop/phoss-smp:$version
+docker tag toop/phoss-smp:$version toop/phoss-smp:latest
+docker push toop/phoss-smp:latest
+docker logout
