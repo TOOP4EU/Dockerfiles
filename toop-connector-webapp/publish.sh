10bfba13d1ac9aac2e283dd8583859447c68edfc
Philip Helger <philip@helger.com>
2020-01-26 20:36:33 +0100

    Added

--- toop-connector-webapp/publish.sh
+++ toop-connector-webapp/publish.sh
@@ -2,8 +2,7 @@
 
 version=0.10.8
 
-docker login
 docker push toop/toop-connector-webapp:$version
 docker tag toop/toop-connector-webapp:$version toop/toop-connector-webapp:latest
 docker push toop/toop-connector-webapp:latest
-docker logout
+
