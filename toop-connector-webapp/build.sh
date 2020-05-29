edea0cd6091858802efc9d001154819d475145d1
yerlibilgin <myildiz83@gmail.com>
2020-05-29 03:07:15 +0300

    changes with respect to tc NG

--- toop-connector-webapp/build.sh
+++ toop-connector-webapp/build.sh
@@ -4,3 +4,4 @@ version=2.0.0-beta4
 
 docker pull tomcat:9-jre11
 docker build --build-arg TOOP_CONNECTOR_VERSION=$version -t toop/tc-webapp:$version .
+docker build --build-arg TOOP_CONNECTOR_VERSION=$version -t toop/tc-webapp:latest .
